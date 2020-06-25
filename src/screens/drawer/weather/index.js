23ce7c7d98b91c6063f68e95a626eed2a7219175
Van Alfred <van@crescode.com>
2020-06-26 04:27:22 +0800

    update

--- src/screens/drawer/weather/index.js
+++ src/screens/drawer/weather/index.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import {View, Text, StyleSheet, FlatList} from 'react-native';
+import {View, Text, StyleSheet, FlatList, Dimensions} from 'react-native';
 import moment from 'moment';
 
 import Baselayout from '../../../components/shared/Baselayout';
@@ -14,24 +14,36 @@ const style = StyleSheet.create({
   },
   th: {borderWidth: 0.5, flex: 1, padding: 5},
   td: {borderWidth: 0.5, flex: 1, padding: 5},
-  text: {textAlign: 'center'},
+  text: {textAlign: 'center', fontSize: 13},
   title: {textAlign: 'center', fontWeight: '500', fontSize: 20},
 });
+const PAGE = Dimensions.get('window');
 const Weather = (props) => {
   const [data, setData] = useState([]);
+  const [windowScreen, setWindowScreen] = useState(PAGE);
   useEffect(() => {
     const forcast = async () => {
       const result = await getWeatherForcast({
         latitude: 8.130604,
         longitude: 125.127655,
       });
-      console.log(result, 'result');
       setData(result.data.list);
       return result;
     };
 
     forcast();
   }, []);
+  const onChange = ({window}) => {
+    setWindowScreen({...window});
+  };
+
+  useEffect(() => {
+    Dimensions.addEventListener('change', onChange);
+    return () => {
+      Dimensions.removeEventListener('change', onChange);
+    };
+  });
+
   return (
     <Baselayout>
       <Text style={style.title}>Weather Forecast</Text>
@@ -42,6 +54,22 @@ const Weather = (props) => {
         <View style={style.th}>
           <Text style={style.text}>Temperature(F)</Text>
         </View>
+        {windowScreen && windowScreen.width > 375 && (
+          <>
+            <View style={style.th}>
+              <Text style={style.text}>Description</Text>
+            </View>
+            <View style={style.th}>
+              <Text style={style.text}>Main</Text>
+            </View>
+            <View style={style.th}>
+              <Text style={style.text}>Pressure</Text>
+            </View>
+            <View style={style.th}>
+              <Text style={style.text}>Humidity</Text>
+            </View>
+          </>
+        )}
       </View>
       <FlatList
         data={data}
@@ -56,6 +84,24 @@ const Weather = (props) => {
               <View style={style.td}>
                 <Text style={style.text}>{item.main.temp}</Text>
               </View>
+              {windowScreen && windowScreen.width > 375 && (
+                <>
+                  <View style={style.td}>
+                    <Text style={style.text}>
+                      {item.weather[0].description}
+                    </Text>
+                  </View>
+                  <View style={style.td}>
+                    <Text style={style.text}>{item.weather[0].main}</Text>
+                  </View>
+                  <View style={style.td}>
+                    <Text style={style.text}>{item.main.pressure}</Text>
+                  </View>
+                  <View style={style.td}>
+                    <Text style={style.text}>{item.main.humidity}</Text>
+                  </View>
+                </>
+              )}
             </View>
           );
         }}
